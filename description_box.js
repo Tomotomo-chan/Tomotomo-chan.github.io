740a560eefd718d567e4b9a9746249fd31b912d6
Tomo-chan <tomotomo-chan@hotmail.com>
2016-08-13 18:19:34 +0200

    pouet

--- description_box.js
+++ description_box.js
@@ -4,6 +4,7 @@ function	show_elem(elem_id)
 	var		new_display;
 
 	old_display = document.getElementById(elem_id).style.display;
+	console.log("old display");
 	console.log(old_display);
 	if ( old_display.localeCompare("none") == 0)
 		new_display = "normal";
