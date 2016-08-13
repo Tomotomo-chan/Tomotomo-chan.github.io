8a6a5cf05da3cdf4ff224a2f08c72e4731791ff4
Tomo-chan <tomotomo-chan@hotmail.com>
2016-08-13 19:35:04 +0200

    pouet

--- age.js
+++ age.js
@@ -4,12 +4,10 @@ function	ft_age(born_date)
 	var	age;
 	var result;
 
-	console.log("coucou 1");
 	birthday = new Date(born_date);
 	age = new Number((new Date().getTime() - birthday.getTime()) / 31536000000).toFixed(0) - 1;
 	result = "Nee le 20/10/1996 (";
 	result = result.concat(age);
 	result = result.concat(" ans)");
-	console.log("result");
 	return result;
 }
--- description_box.js
+++ description_box.js
@@ -4,8 +4,6 @@ function	show_elem(elem_id)
 	var		new_display;
 
 	old_display = document.getElementById(elem_id).style.display;
-	console.log("old display");
-	console.log(old_display);
 	if (old_display.localeCompare("block") == 0)
 		new_display = "none";
 	else
