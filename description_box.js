460bd77c90b122e8bc494ea0b0c7480ad478799d
Tomoko <mathilderessier@hotmail.fr>
2016-08-13 22:11:23 +0200

    css

--- description_box.js
+++ description_box.js
@@ -57,6 +57,7 @@ function	disp_competence(competence, nb_stars)
 			stars = stars.concat("☆");
 		i++;
 	}
+	document.getElementById(competence).
 	document.getElementById(competence).style.color = color;
 	final_str = competence.concat("	");
 	final_str = final_str.concat(stars);
