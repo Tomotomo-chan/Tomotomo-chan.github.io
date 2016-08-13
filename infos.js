d6e738b5ceab396ac8e5677b798a2a6d76abd7fa
Tomo-chan <tomotomo-chan@hotmail.com>
2016-08-13 16:04:51 +0200

    pouet

--- infos.js
+++ infos.js
@@ -1,15 +1,13 @@
 function ft_age(born_date)
 {
 	var birthday;
-	var age;
-	var string;
+	var	age;
+	var result;
 
 	birthday = new Date(born_date);
-	age = new Number((new Date().getTime() - birthday.getTime()) / 31536000000).toFixed(0);
-	
-	string = "Nee le ".concat(born_date);
-	string = string.concat(" (");
-	string = string.concat(age);
-	string = string.concat(")");
-	document.getElementById("age").innerHTML = string;
+	age = new Number((new Date().getTime() - birthday.getTime()) / 31536000000).toFixed(0) - 1;
+	result = "Nee le 20/10/1996 (";
+	result = result.concat(age);
+	result = result.concat(" ans)");
+	return result;
 }
