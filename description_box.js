d538b75bae5597b0b5f01929130f54c5af211b44
Tomo-chan <tomotomo-chan@hotmail.com>
2016-08-13 18:14:50 +0200

    pouet

--- description_box.js
+++ description_box.js
@@ -1,7 +1,13 @@
 function	show_elem(elem_id)
 {
-	var		display;
+	var		old_display;
+	var		new_display;
 
-	display = document.getElementById(elem_id).style.display;
-	console.log(diplay);
+	old_display = document.getElementById(elem_id).style.display;
+	console.log(old_display);
+	if ( display.localeCompare("none") == 0)
+		new_display = "normal";
+	else
+		new_display = "none";
+	document.getElementById(elem_id).style.display = new_display;
 }
