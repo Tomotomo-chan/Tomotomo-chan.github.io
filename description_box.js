3594ce4b66d611b6733a6be953d7823cdb57cbed
Tomoko <mathilderessier@hotmail.fr>
2016-08-13 20:36:49 +0200

    css

--- description_box.js
+++ description_box.js
@@ -10,3 +10,52 @@ function	show_elem(elem_id)
 		new_display = "block";
 	return (new_display);
 }
+
+function	get_color_with_competence(nb_stars)
+{
+	var color;
+
+	if (nb_stars == 5)
+		color = "#66ff66";
+	else if (nb_stars == 4)
+		color = "ccff66";
+	else if (nb_stars == 3)
+		color = "ffff99";
+	else if (nb_stars == 2)
+		color = "ff9966";
+	else if (nb_start == 1)
+		color = "ff6666";
+	else
+		color = "cc0000";
+	return color;
+}
+
+function	check_stars(nb_stars)
+{
+	if (nb_stars < 0)
+		nb_stars = 0;
+	else if (nb_stars > 5)
+		nb_stars = 5;
+	return nb_stars;
+}
+
+function	disp_competence(competence, nb_stars)
+{
+	var		i;
+	var		color;
+	var		string;
+
+	i = 0;
+	nb_stars = check_stars(nb_stars);
+	color = get_color_with_competence(nb_stars);
+	string = "";
+	while (i < 5)
+	{
+		if (i < nb_stars)
+			string = string.concat("★");
+		else
+			string = string.concat("☆");
+	}
+	document.getElementById("competence").style.color = color;
+	document.getElementById("competence").innerHTML = "competence ".concat(string);
+}
