c6b9076e84a0af80c359e0ec9901c5f95a41c663
Tomo-chan <tomotomo-chan@hotmail.com>
2016-08-13 15:45:42 +0200

    pouet

--- infos.js
+++ infos.js
@@ -1,15 +1,16 @@
 function ft_age(born_date)
 {
-	var birthday;
-  	var age;
-  	var string;
+	// var birthday;
+	// var age;
+	var string;
 
-  	birthday = new Date(birthday);
-  	age = new Number((new Date().getTime() - birthday.getTime()) / 31536000000).toFixed(0);
+	// birthday = new Date(birthday);
+	// age = new Number((new Date().getTime() - birthday.getTime()) / 31536000000).toFixed(0);
 	
-  	string = "Nee le ".concat(born_date);
-  	string = string.concat(" (");
-  	string = string.concat(age);
-  	string = string.concat(")");
+	// string = "Nee le ".concat(born_date);
+	// string = string.concat(" (");
+	// string = string.concat(age);
+	// string = string.concat(")");
+	string = "Coucou";
 	document.getElementById("age").innerHTML = string;
 }
