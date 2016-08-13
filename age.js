a97d16b704d3c121e20220e1f5ff2c707e201d4e
Tomoko <mathilderessier@hotmail.fr>
2016-08-13 22:04:31 +0200

    css

--- age.js
+++ age.js
@@ -6,7 +6,7 @@ function	ft_age(born_date)
 
 	birthday = new Date(born_date);
 	age = new Number((new Date().getTime() - birthday.getTime()) / 31536000000).toFixed(0) - 1;
-	result = "Nee le 20/10/1996 (";
+	result = "Née le 20/10/1996 (";
 	result = result.concat(age);
 	result = result.concat(" ans)");
 	return result;
