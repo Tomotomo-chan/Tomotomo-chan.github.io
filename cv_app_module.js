6d1924bcc2649579ba3d60b478d4ae8d755aac4a
Mathilde RESSIER <ressier@stg-interactive.com>
2017-02-07 14:16:35 +0100

    update. TODO: color boxes

--- cv_app_module.js
+++ cv_app_module.js
@@ -1,10 +1,11 @@
 var cv_app = angular.module("CV_App", []);
 
-function		categorie(id, name, description_obj)
+function		categorie(id, name, display_type, description_obj)
 {
 	this.id = id;
 	this.name = name;
 	this.description = description_obj;
+	this.display_type = display_type;
 	this.class_id = id % 3;
 }
 
@@ -29,38 +30,71 @@ function		get_categorie_color(id)
 cv_app.controller("CV_Ctrl", function ($scope)
 {
 	var i = 0;
-	$scope.categorie_list = [new categorie(i += 1, "Langagues de programmation",
+	$scope.categorie_list = [new categorie(i += 1, "Langagues de programmation", "boxes",
 											[
-							 					new description("", "")
+							 					new description("C", "★★★★☆"),
+							 					new description("PHP", "★★★☆☆"),
+							 					new description("UNIX", "★★★☆☆"),
+												new description("Script shell", "★★★☆☆"),
+												new description("HTML CSS", "★★☆☆☆"),
+												new description("Javascript", "★★☆☆☆"),
+												new description("Python", "★★☆☆☆"),
+												new description("MySQL", "★★☆☆☆"),
+												new description("Angular JS", "★★☆☆☆"),
+												new description("J'aimerais faire:", "Node JS")
 											]),
-							 new categorie(i += 1, "Parcours scolaire",
+							 new categorie(i += 1, "Parcours scolaire", "list",
 							 				[
-												new description("Baccalaureat STI2D (*) (mention Tres Bien)", "(*) Sciences et Technologies de l'Industrie et du Développement Durable"),
-												new description("Ecole 42", "\"Sa pédagogie, c’est le peer-to-peer learning : un fonctionnement participatif qui permet aux étudiants de libérer toute leur créativité grâce à l’apprentissage par projets.[...]\"")
+												new description("⚪ Baccalaureat STI2D (*) (mention Tres Bien)", "(*) Sciences et Technologies de l'Industrie et du Développement Durable. Lycée Monge, Savigny sur Orge (91)"),
+												new description("⚪ Ecole 42", "\"Sa pédagogie, c’est le peer-to-peer learning : un fonctionnement participatif qui permet aux étudiants de libérer toute leur créativité grâce à l’apprentissage par projets.[...]\"")
 							 				]),						 
-							 new categorie(i += 1, "Compétences C", 
+							 new categorie(i += 1, "Compétences C", "list",
 							 				[
-							 					new description("", "")
+							 					new description("⚪ Structures de données", "listes chainées, arbre binaire, structures, etc."),
+							 					new description("⚪ Appels système", ""),
+							 					new description("⚪ Backtracking", ""),
+							 					new description("⚪ Itératif/Récursif", ""),
+							 					new description("⚪ Pointeurs sur fonctions", ""),
+							 					new description("⚪ Threads", "thread, mutex, conditions, barrier")
 							 				]),
-							 new categorie(i += 1, "Projets C", 
+							 new categorie(i += 1, "Projets C",  "list",
 							 				[
-							 					new description("", "")
+							 					new description("⚪ Recode de la fonction  printf flags sSpdDioOuUxXcC)", ""),
+							 					new description("⚪ Recode de la fonction ls avec flags", ""),
+							 					new description("⚪ Développement d'un programme utilisant le brute force pour résoudre une problématique", ""),
+							 					new description("⚪ Developement d'une IA de tri optimisé", ""),
+							 					new description("⚪ Developement d'une IA pour un puissance 4 utilisant l'agorithme MinMax", ""),
+							 					new description("⚪ Création d'une bibliothèque personnelle", ""),
+							 					new description("⚪ Développement d'un shell type Bash au complet (affichage avec termcaps)", "")
 							 				]),
-							 new categorie(i += 1, "Projets Web", 
+							 new categorie(i += 1, "Projets Web",  "list",
 							 				[
-							 					new description("", "")
+							 					new description("⚪ Creation du front-end et d'une partie du back-end d'un site de commerce", "(en php: gestion de sessions, mot de passes, produits), rush de 2 jours"),
+							 					new description("⚪ Réalisation d'un site en PHP permettant à des utilisateurs de réaliser et partager des photo-montage ", "Photo montage avec caméra ou photos importées. Implémentation à mains nues des fonctionnalités de base rencontrées sur la majorité des sites possédant une base utilisateur. "),
+							 					new description("⚪ Conception d'un site personnel permettant de gérer des économies", "Projet personnel en développement. HTML/CSS, PHP, Angular JS et MySQL")
 							 				]),
-							 new categorie(i += 1, "Projets/compétences autres", 
+							 new categorie(i += 1, "Projets/compétences autres",  "list",
 							 				[
-							 					new description("", "")
+							 					new description("⚪ Creation de programmes de test unitaires en C et en script shell pour mes projets", ""),
+							 					new description("⚪ Utilisation quotidienne de Git", ""),
+							 					new description("⚪ Gestion des projets avec Trello", "")
 							 				]),
-							 new categorie(i += 1, "Langues", 
+							 new categorie(i += 1, "Langues",  "boxes",
 							 				[
-							 					new description("", "")
+							 					new description("Anglais", "lu couramment, écrit et parlé niveau lycée"),
+							 					new description("Espagnol", "niveau lycée"),
+							 					new description("Japonais", "débutant - apprentissage en auto didacte")
 							 				]),
-							 new categorie(i += 1, "Hobbies", 
+							 new categorie(i += 1, "Hobbies",  "boxes",
 							 				[
-							 					new description("", "")
+							 					new description("L'informatique", ""),
+							 					new description("La culture japonaise", ""),
+							 					new description("Le dessin", ""),
+							 					new description("Voyager", ""),
+							 					new description("Les arts manuels", ""),
+							 					new description("La lecture", "Romans (♡: les Chroniques de l'Armagedon), Mangas..."),
+							 					new description("La musique", "(10 ans de flute traversière)"),
+							 					new description("La gymnastique aux agrès", "")
 							 				])
 							 ]
 
