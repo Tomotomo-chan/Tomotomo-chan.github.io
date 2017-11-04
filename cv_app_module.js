cfd742131d13a8b7905c36fd2634c3e275e4def4
Mathilde RESSIER <ressier@stg-interactive.com>
2017-11-04 17:26:16 +0100

    pouet

--- cv_app_module.js
+++ cv_app_module.js
@@ -4,72 +4,62 @@ cv_app.controller("CV_Ctrl", function ($scope)
 {
 	var i = 0;
 
-	$scope.categorie_list = [new categorie(i += 1, "Langagues de programmation", "boxes",
+	$scope.categorie_list = [new categorie(i += 1, "Langagues de programmation",
+													"★ = connaissances, ★★ = maitrise des principes de bases, ★★★ = bonne maitrise des subtilités du langage, ★★★★ = pro du code !", categorie_display.BOX,
 											[
-							 					new description("C", "★★★★☆"),
-							 					new description("PHP", "★★★☆☆"),
-							 					new description("UNIX", "★★★☆☆"),
-												new description("Script shell", "★★★☆☆"),
-												new description("HTML CSS", "★★☆☆☆"),
-												new description("Javascript", "★★☆☆☆"),
-												new description("Python", "★★☆☆☆"),
-												new description("MySQL", "★★☆☆☆"),
-												new description("Angular JS", "★★☆☆☆"),
-												new description("J'aimerais faire:", "Node JS")
+							 					new content("C", "★★★☆"),
+												new content("Script shell", "★★★☆"),
+							 					new content("UNIX", "★★☆☆"),
+							 					new content("C++", "★★☆☆☆"),
+												new content("Swift", "★★☆☆"),
+												new content("Python", "★☆☆☆"),
+												new content("Front End", "★ HTML/CSS/AngularJS/Angular2"),
+												new content("Back End", "★ NodeJS, PHP"),
+												new content("Base De Données", "★ Mongoose, MySQL")
 											]),
-							 new categorie(i += 1, "Parcours scolaire et expériences professionnelles", "list",
+							 new categorie(i += 1, "Parcours scolaire et expériences professionnelles", "", categorie_display.LIST,
 							 				[
-												new description("⚪ Baccalaureat STI2D (*) (mention Tres Bien)", "(*) Sciences et Technologies de l'Industrie et du Développement Durable. Lycée Monge, Savigny sur Orge (91)"),
-												new description("⚪ Ecole 42", "\"Sa pédagogie, c’est le peer-to-peer learning : un fonctionnement participatif qui permet aux étudiants de libérer toute leur créativité grâce à l’apprentissage par projets.[...]\""),
-												new description("⚪ STG Interactive - Stage de 6 mois (Octobre 2016 - Avril 2017)", "Stage de fin de 1ère année. Compétences: C, shell script, rigueur, travail d'équipe et remise en question")
+												new content("Baccalaureat STI2D (*) (mention Tres Bien)", "(*) Sciences et Technologies de l'Industrie et du Développement Durable. Lycée Monge, Savigny sur Orge (91)"),
+												new content("Ecole 42", "\"Sa pédagogie, c’est le peer-to-peer learning : un fonctionnement participatif qui permet aux étudiants de libérer toute leur créativité grâce à l’apprentissage par projets.[...]\""),
+												new content("STG Interactive - Stage de 6 mois (Octobre 2016 - Avril 2017)", "Stage de fin de 1ère année. Compétences: C, shell script, rigueur, travail d'équipe et remise en question")
 							 				]),						 
-							 new categorie(i += 1, "Compétences C", "list",
+							 new categorie(i += 1, "Compétences", "", categorie_display.BOX,
 							 				[
-							 					new description("⚪ Structures de données", "listes chainées, arbre binaire, structures, etc."),
-							 					new description("⚪ Appels système", ""),
-							 					new description("⚪ Backtracking", ""),
-							 					new description("⚪ Itératif/Récursif", ""),
-							 					new description("⚪ Pointeurs sur fonctions", ""),
-							 					new description("⚪ Threads", "thread, mutex, conditions, barrier")
+							 					new content("Programmation Orientée Objet", "En C++, PHP, javascript, swift et python"),
+							 					new content("Structures de données", "listes chainées, arbre binaire, structures, dictionnaire, tuples, etc."),
+							 					new content("Threads", "thread, mutex, conditions, barrier"),
+							 					new content("API REST", "Framework slim en php, "),
 							 				]),
-							 new categorie(i += 1, "Projets C",  "list",
+							 new categorie(i += 1, "Quelques projets", "", categorie_display.LIST,
 							 				[
-							 					new description("⚪ Recode de la fonction  printf flags sSpdDioOuUxXcC)", ""),
-							 					new description("⚪ Recode de la fonction ls avec flags", ""),
-							 					new description("⚪ Développement d'un programme utilisant le brute force pour résoudre une problématique", ""),
-							 					new description("⚪ Developement d'une IA de tri optimisé", ""),
-							 					new description("⚪ Developement d'une IA pour un puissance 4 utilisant l'agorithme MinMax", ""),
-							 					new description("⚪ Création d'une bibliothèque personnelle", ""),
-							 					new description("⚪ Développement d'un shell type Bash au complet (affichage avec termcaps)", "")
+							 					new content("42sh (C)", "Développement d'un shell type Bash au complet (affichage avec termcaps)"),
+							 					new content("libftASM (assembleur)", "Création d'une petite librairie en assembleur pour découvrir le langage et les principes de très bas niveau."),
+							 					new content("Swifty Companion (Swift - iOS)", "Réalisation d'une application permettant de lister les infos des élèves en utilisant l'API de l'école"),
+							 					new content("Piscine CPP", "Le but est d'aborder les concepts de base de la POO à travers les spécifités du C++"),
+							 					new content("PiggyBox (Angular2/NodeJS/Mongoose)", "Conception d'un site personnel permettant de gérer son budget")
 							 				]),
-							 new categorie(i += 1, "Projets Web",  "list",
+							 new categorie(i += 1, "Outils", "", categorie_display.LIST,
 							 				[
-							 					new description("⚪ Creation du front-end et d'une partie du back-end d'un site de commerce", "(en php: gestion de sessions, mot de passes, produits), rush de 2 jours"),
-							 					new description("⚪ Réalisation d'un site en PHP permettant à des utilisateurs de réaliser et partager des photo-montage ", "Photo montage avec caméra ou photos importées. Implémentation à mains nues des fonctionnalités de base rencontrées sur la majorité des sites possédant une base utilisateur. "),
-							 					new description("⚪ Conception d'un site personnel permettant de gérer des économies", "Projet personnel en développement. HTML/CSS, PHP, Angular JS et MySQL")
+							 					new content("Tests unitaires", "Création systématique de tests unitaires ou e2e (C, script shell, jasmine pour le web et frisby pour les APIS)"),
+							 					new content("Utilisation quotidienne de Git", ""),
+							 					new content("Gestion des projets avec Trello", "")
 							 				]),
-							 new categorie(i += 1, "Projets/compétences autres",  "list",
+							 new categorie(i += 1, "Langues", "", categorie_display.BOX,
 							 				[
-							 					new description("⚪ Creation de programmes de test unitaires en C et en script shell pour mes projets", ""),
-							 					new description("⚪ Utilisation quotidienne de Git", ""),
-							 					new description("⚪ Gestion des projets avec Trello", "")
+							 					new content("Anglais", "lu couramment, écrit et parlé niveau lycée"),
+							 					new content("Espagnol", "niveau lycée"),
+							 					new content("Japonais", "débutant - apprentissage en auto didacte")
 							 				]),
-							 new categorie(i += 1, "Langues",  "boxes",
+							 new categorie(i += 1, "Hobbies", "", categorie_display.BOX,
 							 				[
-							 					new description("Anglais", "lu couramment, écrit et parlé niveau lycée"),
-							 					new description("Espagnol", "niveau lycée"),
-							 					new description("Japonais", "débutant - apprentissage en auto didacte")
-							 				]),
-							 new categorie(i += 1, "Hobbies",  "boxes",
-							 				[
-							 					new description("L'informatique", ""),
-							 					new description("La culture japonaise", ""),
-							 					new description("Le dessin", ""),
-							 					new description("Voyager", ""),
-							 					new description("Les arts manuels", ""),
-							 					new description("La lecture", "Romans, Mangas..."),
-							 					new description("La musique", "(10 ans de flute traversière)"),
-							 					new description("La gymnastique aux agrès", "")
+							 					new content("L'informatique", ""),
+							 					new content("La culture japonaise", ""),
+							 					new content("Le dessin", ""),
+							 					new content("Voyager", ""),
+							 					new content("Les arts manuels", ""),
+							 					new content("La lecture", "Romans, Mangas..."),
+							 					new content("La musique", "(10 ans de flute traversière)"),
+							 					new content("La gymnastique aux agrès", "")
 							 				])
 							 ]
 
@@ -80,9 +70,7 @@ cv_app.controller("CV_Ctrl", function ($scope)
 	{
 		div_id = "#div_" + button_id;
 		console.log(div_id);
-		$('html,body').animate({
-									scrollTop: $(div_id).offset().top
-							   },
+		$('html,body').animate({ scrollTop: $(div_id).offset().top  },
 							   'slow');
 	}
 
