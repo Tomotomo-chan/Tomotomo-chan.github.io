452034443dac8e76346301eaa2f60a39acd8907e
Tomoko <tomotomo-chan@hotmail.com>
2018-01-19 22:34:59 +0100

    modif

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 var app = angular.module('papyApp', []);
 
-app.controller('MainCtrl',
-  function ($scope)
-  {
-      
-  })
+app.controller('MainCtrl', function($scope) {
+    $scope.displayProgram = () => {
+        const titleDiv = document.getElementById('mainTitle');
+        const cardDiv = document.getElementById('mainCard');
+        const validBtn = document.getElementById('confirmBtn');
+
+        titleDiv.style.opacity = 0;
+        titleDiv.style.marginTop = '-900px';
+        setTimeout(() => {
+            titleDiv.style.display = 'none';
+            mainCard.style.display = 'block';
+            confirmBtn.style.display = 'block';
+            setTimeout(() => {
+                mainCard.style.opacity = 1;
+                confirmBtn.style.opacity = 1;
+            }, 500);
+        }, 3000);
+
+        console.log(titleDiv);
+        console.log(cardDiv);
+    };
+});
